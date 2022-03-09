c89242ab0cc5384f2de6907ac22192b6c4700144
CHEE LONG KHOO <khoo@dxc.com>
2022-03-09 15:53:48 +0800

    Underscore_and_asterisk_pattern

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Asterisk_pattern
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int input = 0;
+            string result = "";
+            Console.WriteLine("Please enter a number");
+            input = Convert.ToInt32(Console.ReadLine());
+            result = Draw(input);
+
+            Console.WriteLine(result);
+        }
+
+        public static string Draw(int input)
+        {
+            string output = "";
+            
+            for (int i = 1; i <= input; i++)
+            {
+                
+                for (int j = input; j >= 1; j--)
+                {
+                    if(j <= i)
+                        output += "*";
+                    else
+                        output += "_";
+                    
+
+                }
+                output += "\n";
+            }
+
+            return output;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Csharp_asterisk_pattern
 Asterisk pattern with C#
+
+### The output of the program will be _* depend on the input number by user:
+- If user input 3, then the output will be as following:
+
+__*
+_**
+***
+	
+
+- If user input 5, then the output will be as following:
+
+____*
+___**
+__***
+_****
+*****
+
+- If user input 10, then the output will be as following:
+_________*
+________**
+_______***
+______****
+_____*****
+____******
+___*******
+__********
+_*********
+**********
